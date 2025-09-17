6a2b7f0a5adbc7b9733f68b5d44d3537f603ae0d
Ulises Largaespada <ulisesedu7@gmail.com>
2025-09-16 18:42:03 -0600

    modify the script file

---
+++ script.js
@@ -0,0 +1,14 @@
+function refactoredRefrain() {
+  const refrain = "Twinkle, twinkle, little star,\nHow I wonder what you are!";
+
+  return refrain;
+}
+
+function refactoredSong() {
+  console.log(refactoredRefrain());
+  console.log("Up above the world so high,");
+  console.log("Like a diamond in the sky.");
+  console.log(refactoredRefrain());
+}
+
+refactoredSong();
